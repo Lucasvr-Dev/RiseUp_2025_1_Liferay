64aa92d5971c9ff602b6daba88c738fa4436fb8a
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-10-26 12:22:21 -0300

    Remoção pasta de back end, pois está dando erro

--- back-end_CriacaodeEventos/src/main/java/com/eventos/eventos/repository/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventos.eventos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.eventos.eventos.model.Evento;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
